9387550e82965f8514c33365897a9f63f8917be6
Damian Filipek <damianfilipek81@gmail.com>
2021-02-17 18:13:48 +0100

    Add column component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './Hero.scss';
-import image from './space.png';
+import PropTypes from 'prop-types';
 
-const Hero = () => (
+const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>Things to do </h2>
-    <img className={styles.image} src={image} alt="space"></img>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img className={styles.image} src={props.image} alt="space"></img>
   </header>
 );
+
+Hero.propTypes = {
+  titleText: PropTypes.node.isRequired,
+  image: PropTypes.string,
+};
 
 export default Hero;
