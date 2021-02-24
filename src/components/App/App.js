4d4caa5cb6aa0b9dacebbcd50afb89d18dce96ce
Damian Filipek <damianfilipek81@gmail.com>
2021-02-24 20:24:08 +0100

    Install redux, add redux to components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import styles from './App.scss';
-import Creator from '../Creator/Creator.js';
-import List from '../List/List.js';
+//import Creator from '../Creator/Creator.js';
+import List from '../List/ListContainer.js';
 import PropTypes from 'prop-types';
-import { pageContents, listData, settings } from '../../data/dataStore';
+//import { listData, settings } from '../../data/dataStore';
 
 class App extends React.Component {
   state = {
-    list: this.props.listData || [],
+    lists: this.lists || [],
   }
 
   static propTypes = {
-    listData: PropTypes.array,
+    lists: PropTypes.array,
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
   }
+
   addList(title) {
     this.setState(state => (
       {
-        list: [
-          ...state.list,
+        lists: [
+          ...state.lists,
           {
-            key: state.list.length ? state.list[state.list.length - 1].key + 1 : 0,
+            key: state.lists.length ? state.lists[state.lists.length - 1].key + 1 : 0,
             title,
-            image: listData.image,
+            image: state.lists.image,
             columns: [],
           },
         ],
@@ -30,19 +33,22 @@ class App extends React.Component {
   }
 
   render() {
+    const { title, subtitle, lists } = this.props;
     return (
       <main className={styles.component}>
         <div className={styles.component}>
-          <h1 className={styles.title}>{pageContents.title}</h1>
-          <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-          <List {...listData} />
+          <h1 className={styles.title}>{title}</h1>
+          <h2 className={styles.subtitle}>{subtitle}</h2>
+          {lists.map(listData => (
+            <List key={listData.id} {...listData} />
+          ))}
         </div>
-        <div className={styles.component}>
-          {this.state.list.map(({ key, ...listProps }) => (
-            <List key={key} {...listProps} />
+        {/*<div className={styles.component}>
+          {this.state.lists.map(({ ...listProps }) => (
+            <List key={listData.id} {...listProps} />
           ))}
           <Creator text={settings.listCreatorText} action={title => this.addList(title)} />
-        </div>
+          </div>*/}
       </main>
     );
   }
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,13 +2,10 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-  state = {
-    cards: this.props.cards || [],
-  }
 
   static propTypes = {
     title: PropTypes.string,
@@ -18,31 +15,17 @@ class Column extends React.Component {
     cards: PropTypes.array,
   }
 
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-          },
-        ],
-      }
-    ));
-  }
-
   render() {
+    const { title, cards } = this.props;
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title} <span className={styles.icon}><Icon name={this.props.iconName} /></span></h3>
-        {this.state.cards.map(({ key, ...cardProps }) => (
-          <Card key={key} {...cardProps} />
+        <h3 className={styles.title}>{title} <span className={styles.icon}><Icon name={this.props.iconName} /></span></h3>
+        {cards.map( cardData  => (
+          <Card key={cardData.id} {...cardData} />
         ))}
-        <div className={this.props.creatorClass}>
+        {/*<div className={this.props.creatorClass}>
           <Creator text={this.props.creatorText} action={title => this.addCard(title)} />
-        </div>
+        </div>*/}
       </section>
     );
   }
