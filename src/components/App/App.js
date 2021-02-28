98a065904617cfeae272ceab13627d30e3586d01
Damian Filipek <damianfilipek81@gmail.com>
2021-02-28 20:46:18 +0100

    Change Search redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import styles from './App.scss';
 import List from '../List/ListContainer.js';
 import PropTypes from 'prop-types';
 //import { listData, settings } from '../../data/dataStore';
+import Search from '../Search/SearchContainer.js';
 
 class App extends React.Component {
   state = {
@@ -39,6 +40,7 @@ class App extends React.Component {
         <div className={styles.component}>
           <h1 className={styles.title}>{title}</h1>
           <h2 className={styles.subtitle}>{subtitle}</h2>
+          <Search />
           {lists.map(listData => (
             <List key={listData.id} {...listData} />
           ))}
